6cfec2a3c5e429b78a000f1f52216ee3f99fd0fb
romanbenz147 <benzroman04@gmail.com>
2023-10-14 17:54:06 +0200

    New volume control system

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
+#Video
 import cv2
-import os
 import time
 import mediapipe as mp
-
+from math import hypot
+#Audio
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from comtypes import CLSCTX_ALL
 from ctypes import cast, POINTER
+import numpy as np 
 
 
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+def VolumeControl(length):
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = cast(interface, POINTER(IAudioEndpointVolume))
+    volMin, volMax = volume.GetVolumeRange()[:2]
+    vol = np.interp(length, [15, 220], [volMin, volMax])
+    volume.SetMasterVolumeLevel(vol, None) 
 
-volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-current_volume_level = volume.GetMasterVolumeLevelScalar()
-volume_step = 0.05
-
-def increase_volume():
-    global current_volume_level
-    current_volume_level = min(current_volume_level + volume_step, 1.0)
-    volume.SetMasterVolumeLevelScalar(current_volume_level, None)
-
-def decrease_volume():
-    global current_volume_level
-    current_volume_level = max(current_volume_level - volume_step, 0.0)
-    volume.SetMasterVolumeLevelScalar(current_volume_level, None)
-
-    
-    
 
 def HandTracking():
     cap = cv2.VideoCapture(0)
@@ -43,26 +33,40 @@ def HandTracking():
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         result = hands.process(imgRGB)
+
+        lmList = []
         
         if result.multi_hand_landmarks:
             number_hands = len(result.multi_hand_landmarks)
-            print(number_hands)
             for handLms in result.multi_hand_landmarks:
                 for id, lm in enumerate(handLms.landmark):
                     h, w, c = img.shape
                     cx, cy = int(lm.x * w), int(lm.y * h)
-                    cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
+                    lmList.append([id, cx, cy]) 
+                    #cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
 
-                    mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+                if lmList != []:
+                    x1, y1 = lmList[4][1], lmList[4][2]
+                    x2, y2 = lmList[8][1], lmList[8][2]
+
+                    cv2.circle(img, (x1, y1), 15, (255, 0, 0), cv2.FILLED)  
+                    cv2.circle(img, (x2, y2), 15, (255, 0, 0), cv2.FILLED)
+                    cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+
+                    length = hypot(x2 - x1, y2 - y1)
+                    #VolumeControl(length)
 
 
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
         cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.imshow("Test", img)
+        cv2.imshow("Video", img)
 
-        cv2.waitKey(1)
+        if cv2.waitKey(1) & 0xff == ord("q"):
+            break
 
 
 HandTracking()
