5b36d05d760cebaa077cdc0980a46db67929ae0c
romanbenz147 <benzroman04@gmail.com>
2023-10-14 12:04:27 +0200

    implemented main.py for hand tracking

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
-import openai
+import cv2
+import os
+import time
+import mediapipe as mp
+    
+
+def main():
+    cap = cv2.VideoCapture(0)
+    pTime = 0
+
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands(static_image_mode=False, max_num_hands=1)
+    mpDraw = mp.solutions.drawing_utils
+
+    while True:
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        result = hands.process(imgRGB)
+        if result.multi_hand_landmarks:
+            for handLms in result.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
+
+                    mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS:{int(fps)}', (20, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow("Test", img)
+
+        cv2.waitKey(1)
+
+
 
